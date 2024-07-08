faa4aff81217aa86d384fd91285dcc0124a9d8b2
dendinardiansyahsaputra <91442121+GROWDEN@users.noreply.github.com>
2024-07-08 15:33:13 +0700

    Update Project.js

--- src/pages/admin/Project.js
+++ src/pages/admin/Project.js
@@ -148,9 +148,13 @@ const Project = ({ activities }) => {
         );
 
         if (response.status === 200) {
-          const projectsData = response.data;
+          const projectsData = response.data.map((project) => ({
+            ...project,
+            startdate: convertTimestampToDate(project.startdate),
+            enddate: convertTimestampToDate(project.enddate),
+          }));
           setProjects(projectsData);
-          setFilteredProjects(projectsData); // Set filtered projects initially
+          setFilteredProjects(projectsData);
         }
       } catch (error) {
         console.error("Error fetching projects:", error);
@@ -163,10 +167,10 @@ const Project = ({ activities }) => {
   return (
     <div className="flex h-screen bg-gray-100">
       <Sidebar />
-      <main className="flex-1 w-full">
+      <main className="flex-1 w-full overflow-hidden">
         <Navbar />
-        <div className="px-10 pt-5">
-          <div className="flex justify-between items-center mb-10">
+        <div className="px-10 pt-5 h-full flex flex-col">
+          <div className="flex items-center justify-between mb-8">
             <div className="flex space-x-10">
               <Link to="/Divisi">
                 <h2 className="text-2xl font-bold cursor-pointer">Divisi</h2>
@@ -178,7 +182,7 @@ const Project = ({ activities }) => {
             <div className="relative">
               <input
                 type="text"
-                placeholder="Search Project"
+                placeholder="Cari Project"
                 className="border border-gray-300 rounded px-10 py-2 w-96"
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
@@ -186,32 +190,120 @@ const Project = ({ activities }) => {
               <IoIosSearch className="absolute left-2 top-2 text-gray-400 w-6 h-6" />
             </div>
           </div>
-          <div className="flex flex-wrap justify-start space-x-4">
-            {filteredProjects.map((project, index) => (
-              <Link
-                key={index}
-                to={`/DetailProject/${project.projectId}`}
-                className="relative bg-blue-950 text-white shadow-lg rounded-t-lg flex flex-col justify-between"
-                style={{
-                  height: "150px",
-                  width: "280px",
-                  textDecoration: "none",
-                }}
-              >
-                <div className="p-5 flex justify-center items-center">
-                  <p className="font-bold text-lg">{project.projectname}</p>
-                </div>
-                <div className="flex justify-around items-center flex-grow">
-                  <MdGroups2 className="text-white size-20" />
-                  <p className="text-lg">{project.members.length} Orang</p>
-                </div>
-                <div className="bg-white text-black text-left py-1 rounded-b-lg pl-4 pr-4 flex justify-between items-center">
-                  View Detail
-                  <FaArrowCircleRight />
-                </div>
-              </Link>
-            ))}
+          <div className="mb-8">
+            <button
+              onClick={popupProject}
+              className="bg-blue-950 text-white px-8 py-2 rounded-2xl mb-8"
+            >
+              Tambah Project
+            </button>
           </div>
+          <div className="flex-grow overflow-y-auto">
+            <div className="grid grid-cols-4 gap-10">
+              {filteredProjects?.map((project, index) => (
+                <Link
+                  key={index}
+                  to={`/DetailProject/${encodeURIComponent(
+                    project.projectname
+                  )}`}
+                  className="relative bg-blue-950 text-white shadow-lg rounded-t-lg flex flex-col justify-between"
+                  style={{
+                    height: "150px",
+                    width: "280px",
+                    textDecoration: "none",
+                  }}
+                >
+                  <div className="p-5 flex justify-center items-center">
+                    <p className="font-bold text-lg">{project.projectname}</p>
+                  </div>
+                  <div className="flex justify-around items-center flex-grow">
+                    <MdGroups2 className="text-white size-20" />
+                    <p className="text-lg">{project.members.length} Orang</p>
+                  </div>
+                  <div className="bg-white text-black text-left py-1 rounded-b-lg pl-4 pr-4 flex justify-between items-center">
+                    View Detail
+                    <FaArrowCircleRight />
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </div>
+          {isLogPopupVisible && (
+            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+              <div className="w-max h-max bg-white p-7 rounded-3xl relative">
+                <button
+                  onClick={closePopup}
+                  className="absolute top-6 right-9 text-gray-400 hover:text-gray-600"
+                >
+                  ✕
+                </button>
+                <div className="mb-3 text-start font-bold text-lg">
+                  Tambah Project Baru
+                </div>
+                <label>Nama Project</label>
+                <input
+                  name="projectname"
+                  value={newProject.projectname}
+                  onChange={handleInputChange}
+                  className="w-full p-2 border border-gray-300 rounded-lg mt-1 mb-3"
+                  placeholder="Masukkan Nama Project"
+                />
+                <label>Anggota Project</label>
+                <div className="mt-1 mb-3">
+                  <Select
+                    isMulti
+                    closeMenuOnSelect={false}
+                    hideSelectedOptions={false}
+                    components={{ Option: CheckboxOption }}
+                    onChange={handleChange}
+                    options={employeeData}
+                    allowSelectAll={true}
+                    value={selectedEmployees}
+                    placeholder="Pilih karyawan"
+                  />
+                </div>
+                <div className="flex justify-between">
+                  <div className="w-full mr-3">
+                    <label>Tanggal Mulai</label>
+                    <br />
+                    <input
+                      name="startdate"
+                      value={newProject.startdate}
+                      onChange={handleInputChange}
+                      className="w-full p-2 border border-gray-300 rounded-lg mt-1 mb-3"
+                      type="date"
+                    />
+                  </div>
+                  <div className="w-full">
+                    <label>Tanggal Akhir</label>
+                    <br />
+                    <input
+                      name="enddate"
+                      value={newProject.enddate}
+                      onChange={handleInputChange}
+                      className="w-full p-2 border border-gray-300 rounded-lg mt-1 mb-3"
+                      type="date"
+                    />
+                  </div>
+                </div>
+                <label>Deskripsi Project</label>
+                <textarea
+                  name="description"
+                  value={newProject.description}
+                  onChange={handleInputChange}
+                  className="w-full p-2 border border-gray-300 rounded-lg mt-1 mb-3"
+                  placeholder="Masukkan Deskripsi Project"
+                  rows="4"
+                />
+                <button
+                  onClick={handleConfirmProject}
+                  className="bg-blue-950 text-white px-4 py-2 rounded-lg"
+                >
+                  Konfirmasi
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       </main>
     </div>
